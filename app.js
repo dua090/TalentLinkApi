097848e8f66b82e95bc74eb26369b3733041af84
dua090 <duamanu2003@gmail.com>
2026-05-06 20:25:25 +0530

    updated search controller

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/candidates", require("./routes/candidateRoutes"));
-<<<<<<< HEAD
-app.use("/api/search", require("./routes/candidateRoutes"));
-=======
 app.use("/api/analytics", analyticsRoutes);
->>>>>>> 4bdb8b1a94f2858d4becf51e62bdfd76fa0c8e9d
+app.use("/api/search", require("./routes/candidateRoutes"));
 
 module.exports = app;
