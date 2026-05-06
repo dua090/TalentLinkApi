f74e296a2619cbb007629646111d3b43440389da
mohammadmohiddin-dotcom <mohammad.mohiddin@globallogic.com>
2026-05-06 14:50:26 +0530

    Updated Candidate
    # Conflicts:
    #	controllers/candidateController.js

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -3,12 +3,14 @@ const multer = require("multer");
 const auth = require("../middleware/authMiddleware");
 const {
   uploadResume,
-  searchCandidates
+  searchCandidates,
+  getCandidates
 } = require("../controllers/candidateController");
 
 const upload = multer({ dest: "uploads/" });
 
 router.post("/upload", auth, upload.single("resume"), uploadResume);
 router.get("/search", auth, searchCandidates);
+router.get("/", getCandidates);
 
 module.exports = router;
